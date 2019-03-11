aa95b56882fb216cd387f73848b9884c1df832eb
Chandler Willman <Chandler@Chandlers-MacBook-Pro-3.local>
2019-03-10 18:54:46 -0600

    Finished, but it doesn't work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HistoryModal from './components/HistoryModal';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -17,12 +18,22 @@ class App extends Component {
 
   
   componentDidMount() {
-    //create request here
+    axios.get('/api/messages').then(res => {
+      this.setState({
+        allMessages: res.data
+      });
+    });
   }
 
-  // CREATE MESSAGE METHOD
+  createMessage() {
+    let { username, message } = this.state;
+    axios
+      .post('/api/messages', { username: username, message: message })
+      .then(res => {
+        this.setState({ allMessages: res.data });
+      });
+  }
   
-
   saveUsername() {
     if (this.state.username) {
       this.setState({ messageInputDisabled: !this.state.messageInputDisabled });
